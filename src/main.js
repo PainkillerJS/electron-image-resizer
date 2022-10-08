aa37b2f524aaf615692088035e8ff6f5f22e8a6a
Yuriy <nikorezar@gmail.com>
2022-10-08 17:01:49 +0300

    feat: add style and img

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
+const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
 
 const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     title: 'Image Resizer',
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, './render/index.html'));
 };
