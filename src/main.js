d3c587c1398cd273316cc5ed08799824576d656c
Yuriy <nikorezar@gmail.com>
2022-10-08 17:23:27 +0300

    feat: add second window (about)

--- src/main.js
+++ src/main.js
@@ -4,18 +4,6 @@ const { app, BrowserWindow, Menu } = require('electron');
 const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
 
-const menu = [
-  {
-    label: 'File',
-    submenu: [
-      {
-        label: 'Quit',
-        click: () => app.quit(),
-      },
-    ],
-  },
-];
-
 const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     title: 'Image Resizer',
@@ -29,6 +17,54 @@ const createMainWindow = () => {
 
   mainWindow.loadFile(path.join(__dirname, './render/index.html'));
 };
+
+const createAboutWindow = () => {
+  const aboutWindow = new BrowserWindow({
+    title: 'About Image Resizer',
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, './render/about.html'));
+};
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        click: () => app.quit(),
+      },
+    ],
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+];
 
 app.whenReady().then(() => {
   createMainWindow();
